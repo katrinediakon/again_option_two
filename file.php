2a17f1111724921050fa320509451cf38d0b09e1
Kate <Katrinekatek@gmail.com>
2019-09-18 15:37:26 +0300

    ex4

---
+++ file.php
@@ -0,0 +1,8 @@
+<?php
+
+if(date('H')>="9" && date('H')<="18" )
+echo '<a href="tel:84952128506" class="phone">8 (495) 212-85-06</a>';
+else
+echo '<a href="mailto:store@store.ru" class="phone">store@store.ru</a>';
+
+ ?>
